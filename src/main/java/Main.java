b7863b65bfd2e85ca83e9af13c9b7e2e02996185
dizsart <darogadiba@gmail.com>
2021-07-27 11:29:49 +0100

    Add stream classes and examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -10,56 +9,59 @@ public class Main {
 
 
 //FUNCTIONAL
-        String[] functionalCountries = {"India", "Australia", "England", "South Africa", "Srilanka", "Newzealand", "West Indies", "Scotland"};
-        Function<String[], String> converter = (all) -> {
-            String names = "";
-            for(String n : all) {
-                String result = n.substring(0, n.indexOf(""));
-                result = n.substring(n.indexOf("")) + " " + result;
-                names += result + "\n";
-            }
-            return names;
-        };
-        System.out.println(converter.apply(functionalCountries));
-
+//        String[] functionalCountries = {"India", "Australia", "England", "South Africa", "Srilanka", "Newzealand", "West Indies", "Scotland"};
+//        Function<String[], String> converter = (all) -> {
+//            String names = "";
+//            for(String n : all) {
+//                String result = n.substring(0, n.indexOf(""));
+//                result = n.substring(n.indexOf("")) + " " + result;
+//                names += result + "\n";
+//            }
+//            return names;
+//        };
+//        System.out.println(converter.apply(functionalCountries));
+//
 
 
 //SUPPLIER
         String product = "together";
-        double price = 133.342;
-        Supplier<Boolean> boolSupplier = () -> product.length() == 10;
-        Supplier<Integer> integerSupplier = () -> product.length() - 2;
-        Supplier<String> supplier = () -> product.toUpperCase();
+//        double price = 133.342;
+        Supplier<Boolean> boolSupplier = () ->  product.length() == 10;
+//        Supplier<Integer> integerSupplier = () -> product.length() - 2;
+//        Supplier<String> supplier = () -> product.toUpperCase();
         BooleanSupplier booleanSupplier = () -> product.length() == 10;
-        IntSupplier intSupplier = () -> product.length() - 2;
-        DoubleSupplier doubleSupplier = () -> price -20;
+//        IntSupplier intSupplier = () -> product.length() - 2;
+//        DoubleSupplier doubleSupplier = () -> price -20;
         LongSupplier longSupplier = () -> new Date().getTime();
-
-
-        System.out.println(booleanSupplier.getAsBoolean());
-        System.out.println(integerSupplier.get());
-        System.out.println(doubleSupplier.getAsDouble());
+//
+//
+//        System.out.println(booleanSupplier.getAsBoolean());
+//        System.out.println(integerSupplier.get());
+//        System.out.println(doubleSupplier.getAsDouble());
         System.out.println(longSupplier.getAsLong());
-        System.out.println(supplier.get());
-
-        System.out.println(boolSupplier.get());//false
-        System.out.println(intSupplier.getAsInt());//5
-        System.out.println(supplier.get());//ANDROID
-
-
-
-//CONSUMER
-
-        Consumer<Integer> c1 = i -> System.out.println(5*i);
-        Consumer<Integer> c2 = i -> System.out.println(5+i);
-
-        c1.andThen(c2).accept(4); // 20, 9
-        System.out.println("");
-        c2.andThen(c1).accept(5); //10, 25
-        System.out.println("");
-        c1.andThen(i -> System.out.println(3*i)).accept(4);
+//        System.out.println(supplier.get());
+//
+//        System.out.println(boolSupplier.get());
+//        System.out.println(intSupplier.getAsInt());
+//        System.out.println(supplier.get());
 
 
+
+////CONSUMER
+//
+//        Consumer<Integer> multiplyValue = i -> System.out.println(5*i);
+//        Consumer<Integer> addValue = i -> System.out.println(5+i);
+//
+//        multiplyValue.andThen(addValue).accept(4);
+//        System.out.println("");
+//        addValue.andThen(multiplyValue).accept(5);
+//        System.out.println("");
+//        multiplyValue.andThen(i -> System.out.println(3*i)).accept(4);
+
+
+
+
+        
 //PREDICATE
 
         Predicate<Integer> p = number -> (number % 2 == 0);
---
+++ src/main/java/Streams3.java
@@ -0,0 +1,24 @@
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+
+public class Streams3 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+
+        IntStream.range(0, 10)
+            .forEach(i -> {
+                if (i % 2 == 1) System.out.println(i);
+            });
+
+        IntStream.range(0, 10)
+            .filter(i -> i % 2 == 1)
+            .forEach(System.out::println);
+
+    }
+}
